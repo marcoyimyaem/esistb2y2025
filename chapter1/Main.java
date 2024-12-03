fc1e3b5292122f17cc593b741bed40c8c3576ea6
marcoyimyaem <marcoyimyaem@gmail.com>
2024-12-03 12:06:27 +0000

    basic part 2

--- chapter1/Main.java
+++ chapter1/Main.java
@@ -1,6 +1,10 @@
 // sample file
 package chapter1;
 
+import java.util.*;
+
+
+
 public class Main{
     public static void main(String[] args) {
         //create and instance of a Toy class = Toy object
@@ -16,5 +20,9 @@ public class Main{
         toy1.price = 4500;
         toy1.quantity = 12;
         System.out.println();
+        Random r = new Random();
+        System.out.println(r.nextInt(100));
+        ArrayList al = new ArrayList<>();
+        Date date1 = new Date();
     }
 }
